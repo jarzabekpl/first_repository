9bcd296128854d75c07c552aa4d8e3baae275381
jarzabekpl <itjarzebowski.m@gmail.com>
2022-10-02 23:15:09 +0200

    Add initial files

---
+++ README.md
@@ -0,0 +1,4 @@
+# 
+
+A Pen created on CodePen.io. Original URL: [https://codepen.io/jarzabek/pen/poVVOeo](https://codepen.io/jarzabek/pen/poVVOeo).
+
---
+++ src/script.js
@@ -0,0 +1,7 @@
+let names = ["Mateusz", "Emilia"];
+names.push("Radek");
+names.unshift("Iza");
+
+for (let i = 0; i < names.length; i++) {
+  console.log(names[i]);
+}
